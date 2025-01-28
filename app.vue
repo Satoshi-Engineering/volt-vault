cc6f45353e96668daf534c9428a4311a8feba8e8
thespielplatz <informatics@gmx.net>
2025-01-28 13:21:02 +0100

    chore: add info page
    Refs: projects#1494

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Volt Vault</h1>
+    <p>
+      Volt Vault is Satoshi Engineering's internal project, focused on 
+      implementing essential features missing in the Lightning Network
+      landscape, bridging gaps to enhance functionality and usability.
+    </p>
   </div>
 </template>
