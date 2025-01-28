9a2ebfa88612c3c9c570c123662276212567edf2
thespielplatz <informatics@gmx.net>
2025-01-28 13:24:17 +0100

    ci: add linting
    Refs: projects#1494

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Volt Vault</h1>
     <p>
-      Volt Vault is Satoshi Engineering's internal project, focused on 
+      Volt Vault is Satoshi Engineering's internal project, focused on
       implementing essential features missing in the Lightning Network
       landscape, bridging gaps to enhance functionality and usability.
     </p>
