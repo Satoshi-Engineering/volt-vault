4fe4082b1bb7923b758364a4c7517166ed8f344b
thespielplatz <informatics@gmx.net>
2025-01-28 13:36:52 +0100

    chore: add nuxt/ui and Github Badge
    Refs: projects#1494

---
+++ app.config.ts
@@ -0,0 +1,8 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'purple',
+    container: {
+      constrained: 'max-w-2xl',
+    }
+  }
+})
--- app.vue
+++ app.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    <h1>Volt Vault</h1>
-    <p>
-      Volt Vault is Satoshi Engineering's internal project, focused on
-      implementing essential features missing in the Lightning Network
-      landscape, bridging gaps to enhance functionality and usability.
-    </p>
+  <div class="pt-10">
+    <UContainer>
+      <UCard>
+        <template #header>
+          <div class="font-bold text-2xl">Volt Vault</div>
+        </template>
+
+        Volt Vault is Satoshi Engineering's internal project, focused on
+        implementing essential features missing in the Lightning Network
+        landscape, bridging gaps to enhance functionality and usability.
+
+        <template #footer>
+          <GithubLink />
+        </template>
+      </UCard>
+    </UContainer>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
