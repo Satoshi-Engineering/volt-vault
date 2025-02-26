912cf2e9fedfd2a30e3dd09a00b63292d346797a
thespielplatz <informatics@gmx.net>
2025-02-26 16:34:21 +0100

    style: remove function wide try catch
    Refs: projects#1527

--- server/api/lnd/query-routes.get.ts
+++ server/api/lnd/query-routes.get.ts
@@ -8,24 +8,16 @@ export default defineEventHandler(async (event) => {
   const { paymentRequestEncoded } = await getValidatedQuery(event, InputSchema.parse)
   const grpcClient = useGrpc()
 
-  try {
-    const paymentRequestDecoded = await grpcClient.decodePaymentRequest({
-      pay_req: paymentRequestEncoded,
-    })
-    const queryRoutesResponse = await grpcClient.queryRoutes({
-      pub_key: paymentRequestDecoded.destination,
-      amt: Number(paymentRequestDecoded.num_satoshis),
-      route_hints: paymentRequestDecoded.route_hints,
-    })
-    return {
-      paymentRequestDecoded,
-      queryRoutesResponse,
-    }
-  } catch (error) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: `${error}`,
-      message: `${JSON.stringify(error)}`,
-    })
+  const paymentRequestDecoded = await grpcClient.decodePaymentRequest({
+    pay_req: paymentRequestEncoded,
+  })
+  const queryRoutesResponse = await grpcClient.queryRoutes({
+    pub_key: paymentRequestDecoded.destination,
+    amt: Number(paymentRequestDecoded.num_satoshis),
+    route_hints: paymentRequestDecoded.route_hints,
+  })
+  return {
+    paymentRequestDecoded,
+    queryRoutesResponse,
   }
 })
